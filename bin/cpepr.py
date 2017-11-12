ecdf88b682e39f883bed507044a6e099ddc49370
Yuta Hinokuma <yuta@abeja.asia>
2017-11-12 12:07:47 +0900

    executable

--- bin/cpepr.py
+++ bin/cpepr.py
@@ -5,10 +5,13 @@ import datetime
 import random
 import click
 from jinja2 import Template
-from collections import named_tuple
+from collections import namedtuple
 
 
-Problem = named_tuple('Problem', ['id_', 'url', 'samples'])
+ASSET_DIR = '.'
+WORK_DIR = 'workdir'
+
+Problem = namedtuple('Problem', ['id_', 'url', 'in_', 'out_'])
 
 
 def get_default_language():
@@ -25,36 +28,43 @@ def set_default_language(language):
 
 
 def get_language_extension(language):
-    return open(os.path.join('language_templates', language, 'extension.txt')).read()
+    return open(os.path.join(ASSET_DIR, 'language_templates', language, 'extension.txt')).read().strip()
+
 
 def get_random_problem():
-    problems = json.load(open('problems.json'))
-    problem_dict = random.choice(problems)
+    problems_dir = os.path.join(ASSET_DIR, 'problems')
+    problems = list(map(lambda x: os.path.join(problems_dir, x), os.listdir(problems_dir)))
+    problem_dir = random.choice(problems)
+    problem_information = json.load(open(os.path.join(problem_dir, 'information.json')))
 
-    return Problem(problem_dict['id'], problem_dict['url'], problem_dict['samples'])
+    return Problem(
+        id_=problem_information['id'], url=problem_information['url'],
+        in_=open(os.path.join(problem_dir, 'in.txt')).read(),
+        out_=open((os.path.join(problem_dir, 'in.txt'))).read()
+    )
 
 
 def get_template_string(language):
     try:
-        return open(os.path.join('language_templates', language, 'source.j2')).read()
+        return open(os.path.join(ASSET_DIR, 'language_templates', language, 'source.j2')).read()
     except IOError:
         print('language template is not found')
         sys.exit(-1)
 
 
 def create_scaffold_for_solve(problem, source, extension, now):
-    base_dir = os.path.join('.', now, problem.id_)
+    base_dir = os.path.join(WORK_DIR, now, problem.id_)
     os.makedirs(base_dir)
 
     filename = os.path.join(base_dir, 'prog.{}'.format(extension))
     print('created {}'.format(filename))
     open(filename, 'w').write(source)
-    filename = os.path.join(base_dir, 'in.txt', 'w')
+    filename = os.path.join(base_dir, 'in.txt')
     print('created {}'.format(filename))
-    open(filename).write(problem.samples['in'])
-    filename = os.path.join(base_dir, 'out.txt', 'w')
+    open(filename, 'w').write(problem.in_)
+    filename = os.path.join(base_dir, 'out.txt')
     print('created {}'.format(filename))
-    open(filename).write(problem.samples['out'])
+    open(filename, 'w').write(problem.out_)
 
 
 @click.group(invoke_without_command=True)
@@ -64,10 +74,9 @@ def cli(ctx):
         print(ctx.get_help())
 
 
-@cli.command()
-@click.option('language', 'l')
-def fetch_problem(language):
-    language = get_default_language() if language is None else language
+@cli.command(help="test")
+def fetch_problem():
+    language = get_default_language()
     problem = get_random_problem()
 
     template_string = get_template_string(language)
@@ -81,3 +90,6 @@ def fetch_problem(language):
     })
 
     create_scaffold_for_solve(problem, source, get_language_extension(language), now)
+
+if __name__ == '__main__':
+    cli()
